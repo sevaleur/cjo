3d0ee314f24fb7713b056bf542decd811832769f
Sune Valeur <sunevaleur@gmail.com>
2022-12-10 21:40:03 +0100

    pug + express integration

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) =>
+{
+    res.render('pages/home')
+})
+
+app.get('/about', (req, res) =>
+{
+    res.render('pages/about')
+})
+
+app.get('/work', (req, res) =>
+{
+    res.render('pages/work')
+})
+
+app.get('/detail/:id', (req, res) =>
+{
+    res.render('pages/detail')
+})
+
+app.listen(port, () =>
+{
+    console.log(`Listening at http://localhost:${port}`)
+})
